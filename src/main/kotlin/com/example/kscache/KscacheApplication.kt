a0aef4107c6dfe5b84ce2fe9a6135d0bbe44abe5
phodo <phodo@phodobit.kr>
2023-09-03 18:02:43 +0900

    initialize spring boot project

---
+++ src/main/kotlin/com/example/kscache/KscacheApplication.kt
@@ -0,0 +1,11 @@
+package com.example.kscache
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KscacheApplication
+
+fun main(args: Array<String>) {
+    runApplication<KscacheApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/kscache/KscacheApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.kscache
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KscacheApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
